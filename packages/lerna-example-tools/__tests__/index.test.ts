a5b2cb2701339a4fee59882c6260f09ff86e0116
wangjun <baige@tuya.com>
2019-12-05 11:52:15 +0800

    feat(lerna-example-tools): add Multi functions

--- packages/lerna-example-tools/__tests__/index.test.ts
+++ packages/lerna-example-tools/__tests__/index.test.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { Add, AddAll } from '../src/index';
+import {Add, AddAll, Multi, MultiAll,} from '../src';
 
 describe('@wangjuncode/lerna-example-tools', () => {
     it('should be 3', function () {
@@ -9,5 +9,12 @@ describe('@wangjuncode/lerna-example-tools', () => {
 
     it('should be 6', function () {
         expect(AddAll(1, 2, 3)).toBe(6);
+    });
+
+    it('should be 1', function () {
+        expect(Multi(1, 1)).toBe(1);
+    });
+    it('should be 2', function () {
+        expect(MultiAll(1, 2)).toBe(2);
     });
 });
--- packages/lerna-example-tools/src/index.ts
+++ packages/lerna-example-tools/src/index.ts
@@ -1,7 +1,12 @@
 const Add = (a: number, b: number): number => a + b;
 const AddAll = (...numbers: number[]): number => numbers.reduce((pre: number, cur: number) => pre + cur);
 
+const Multi = (a: number, b: number): number => a*b;
+const MultiAll = (...numbers: number[]): number => numbers.reduce((pre: number, cur: number) => pre * cur);
+
 export  {
   Add,
   AddAll,
+  Multi,
+  MultiAll
 };
