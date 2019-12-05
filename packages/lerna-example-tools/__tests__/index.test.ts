82a2b45eb07dd5258088ae502b6e6060f1ec2f43
wangjun <baige@tuya.com>
2019-12-05 14:22:54 +0800

    fix(tools): fix lint
    affects: @wangjuncode/lerna-example-tools

--- packages/lerna-example-tools/__tests__/index.test.ts
+++ packages/lerna-example-tools/__tests__/index.test.ts
@@ -1,20 +1,18 @@
-'use strict';
-
-import {Add, AddAll, Multi, MultiAll,} from '../src';
+import {Add, AddAll, Multi, MultiAll} from '../src';
 
 describe('@wangjuncode/lerna-example-tools', () => {
-    it('should be 3', function () {
+    it('should be 3', () => {
         expect(Add(1, 2)).toBe(3);
     });
 
-    it('should be 6', function () {
+    it('should be 6', () => {
         expect(AddAll(1, 2, 3)).toBe(6);
     });
 
-    it('should be 1', function () {
+    it('should be 1', () => {
         expect(Multi(1, 1)).toBe(1);
     });
-    it('should be 2', function () {
+    it('should be 2', () => {
         expect(MultiAll(1, 2)).toBe(2);
     });
 });
